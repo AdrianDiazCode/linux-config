3b2cf9d8df8da3cb220137d74b2ad0be989c70d8
Adrian <eidiazcas@gmail.com>
2025-09-25 06:18:57 -0500

    deps and init shell

--- .bash-init.sh
+++ .bash-init.sh
@@ -1,10 +1,13 @@
+tmux source ~/linux-config/.tmux.conf
 tmux
 
-bindkey -v
+command -v bindkey &>/dev/null && bindkey -v
 
-alias bm='cd $HOME/Documents/buildman/buildman-k8 && source source.sh && source tmux-session.sh'
+alias bm='cd ~/Documents/buildman/buildman-k8 && source source.sh && source tmux-session.sh'
 alias nvimconf='source  ~/.config/nvim/tmux-session.sh'
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
---
+++ install-deps.sh
@@ -0,0 +1,71 @@
+# Define variables for colors
+RED='\e[31m'
+GREEN='\e[32m'
+RESETCOLOR='\e[0m' # No Color
+
+# Example usage
+
+echo -e "${GREEN}"
+echo -e "\n Installing curl..."
+echo -e "${RESETCOLOR}"
+apt-get install curl
+echo "curl version check: $(curl --version)"
+
+echo -e "${GREEN}"
+echo -e "\n Installing Homebrew..."
+echo -e "${RESETCOLOR}"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >>~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "brew version check: $(brew -v)"
+
+echo -e "${GREEN}"
+echo -e "\n Installing xclip..."
+brew install xclip
+
+echo -e "${GREEN}"
+echo -e "\n Installing Neovim..."
+echo -e "${RESETCOLOR}"
+brew install neovim
+echo "nvim version check: $(nvim -v)"
+
+echo -e "${GREEN}"
+echo -e "\n Installing NVM..."
+echo -e "${RESETCOLOR}"
+brew install nvm
+echo "nvm version check: $(nvm -v)"
+
+echo -e "${GREEN}"
+echo -e "\n Installing tmux..."
+echo -e "${RESETCOLOR}"
+brew install tmux
+echo "tmux version check: $(tmux -V)"
+
+echo -e "${GREEN}"
+echo -e "\n Installing zig"
+echo -e "${RESETCOLOR}"
+brew install zig
+echo "zig version check: $(zig version)"
+
+# if ! command -v gcc &>/dev/null; then
+#   alias gcc='gcc-15'
+#   echo "alias gcc='gcc-15'" >>./.bash-init.sh
+# fi
+
+echo -e "${GREEN}"
+echo -e "\n Installing Git..."
+echo -e "${RESETCOLOR}"
+brew install git
+echo "git version check: $(git -v)"
+
+echo -e "${GREEN}"
+echo -e "\n Installing fzf..."
+echo -e "${RESETCOLOR}"
+brew install fzf
+echo "fzf version check: $(fzf --version)"
+
+echo -e "${GREEN}"
+echo -e "\n Installing lazygit..."
+echo -e "${RESETCOLOR}"
+brew install lazygit
+echo "lazygit version check: $(lazygit --version)"
