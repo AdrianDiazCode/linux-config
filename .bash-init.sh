d87fc1bc97250466c0415f882d40b0bed28c22e4
Adrian Diaz <Adrian.Castaneda.-ND@disney.com>
2025-09-25 01:16:40 -0500

    init

---
+++ .bash-init.sh
@@ -0,0 +1,10 @@
+tmux
+
+bindkey -v
+
+alias bm='cd $HOME/Documents/buildman/buildman-k8 && source source.sh && source tmux-session.sh'
+alias nvimconf='source  ~/.config/nvim/tmux-session.sh'
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
