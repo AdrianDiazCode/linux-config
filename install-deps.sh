38ba2284af11dddb4bdd133bbfbf252535b290e0
Adrian <eidiazcas@gmail.com>
2025-09-25 08:38:50 -0500

    more

--- install-deps.sh
+++ install-deps.sh
@@ -71,12 +71,30 @@ echo -e "${RESETCOLOR}"
 brew install ripgrep
 echo "rigpgrep version check: $(rg --version)"
 
-echo -e "${GREEN}"
-echo -e "\n Installing docker..."
-echo -e "${RESETCOLOR}"
-brew install docker
-sudo systemctl enable docker.service
-echo "docker version check: $(docker --version)"
+if command -v apt-get &>/dev/null; then
+  echo -e "${GREEN}"
+  echo -e "\n Installing docker..."
+  echo -e "${RESETCOLOR}"
+  # Add Docker's official GPG key:
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo systemctl start docker
+  sudo systemctl enable docker
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  echo "docker version check: $(docker --version)"
+fi
 
 echo -e "${GREEN}"
 echo -e "\n Installing docker-compose..."
